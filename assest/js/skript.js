4ef0a5f80bd434732beda485a34de456f7fb2e07
Anis5221 <parsonal494@gmail.com>
2022-02-21 18:27:35 +0600

    project to git

---
+++ assest/js/skript.js
@@ -0,0 +1,26 @@
+(function($) {
+    "use strict"
+
+// slider js 
+$('.slider_start').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  infinite: true,
+  slidesToShow: 1,
+  dots: true,
+  // fade: true,
+  prevArrow: '<button type="button" class="slick-prev fa-solid fa-chevron-left"></button>',
+  nextArrow: '<button type="button" class="slick-next fa-solid fa-chevron-right"></button>',
+
+});
+// progress
+// end single product  zoom image 
+// date picker 
+// $(function() {
+//   $( "#datepicker" ).datepicker();
+//   });
+  
+// date picker 
+
+})(jQuery);
+
